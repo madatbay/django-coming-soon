2f7c1c057db10eb42e7fd5fc5385c73c4f191b2b
Madat Bay <medet.bayramov22@gmail.com>
2020-11-04 21:11:08 +0400

    collectstatic error

--- coming_soon/settings.py
+++ coming_soon/settings.py
@@ -125,8 +125,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
